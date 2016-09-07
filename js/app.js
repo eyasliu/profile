1de05b0c6d541bf587caea1f5ebf776090848086
Eyas <liuyuesongde@163.com>
2016-09-07 22:59:30 +0800

    add cache

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 function App(){
 	var that = this;
+  this.wasFetch = false;
 	this.domReady(function(){
 		that.init()
 	})
@@ -25,20 +26,48 @@ App.prototype.domReady = (function (cb){
   }
 })()
 
-App.prototype.init = function(){
-	window.addEventListener('resize', this.resize)
-	this.resize();
-	fetch('/README.md')
-		.then(function(res){ return res.text() })
-		.then(function(text){
-			var content = document.getElementById('content');
-			content.innerHTML = marked(text)
-		})
-}
-
 App.prototype.resize = function(){
 	var header = document.getElementById('header')
   header.style.height = (window.innerHeight - 150) + 'px'
 }
+
+App.prototype.bindEvent = function(){
+  window.addEventListener('resize', this.resize)
+  this.resize();
+  
+}
+
+App.prototype.fetch = function(cb){
+  fetch('/README.md')
+    .then(function(res){ return res.text() })
+    .then(function(text){
+      this.wasFetch = true;
+      localStorage.setItem('content', text);
+      cb(text);
+    }.bind(this))
+  
+}
+
+App.prototype.updateCache = function(){
+  this.fetch(localStorage.setItem.bind(localStorage, 'content'))
+}
+
+App.prototype.render = function(text){
+  if(text){
+    var content = document.getElementById('content');
+    content.innerHTML = marked(text)
+    !this.wasFetch && this.updateCache();
+  } else {
+    var storageText = localStorage.getItem('content');
+    storageText ? arguments.callee.call(this, storageText) : this.fetch(arguments.callee);
+  }
+}
+
+App.prototype.init = function(){
+  var that = this;
+  this.bindEvent();
+  this.render();
+}
+
 
 var app = new App()
